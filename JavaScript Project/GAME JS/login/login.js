0b0e8cd8cd5686b401e7e4da246784468e92546c
unknown <mohinim837@gmail.com>
2025-02-25 10:28:00 +0530

    file added

--- JavaScript Project/GAME JS/login/login.js
+++ JavaScript Project/GAME JS/login/login.js
@@ -1,3 +1,15 @@
+let btn = document.getElementById('show');
+btn.addEventListener('click', () => {
+    let inp = document.getElementById('loginPassword')
+    if (inp.type === 'password') {
+        inp.type = "text";
+        btn.textContent = 'Hide'
+    }
+    else {
+        inp.type = 'password'
+        btn.textContent = "Show"
+    }
+})
 document.getElementById("loginForm").addEventListener("submit", (e) => {
         e.preventDefault();
     
@@ -17,6 +29,6 @@ document.getElementById("loginForm").addEventListener("submit", (e) => {
             localStorage.setItem("Login",JSON.stringify(users))
             window.location.href="../home/game.html"
         } else {
-            alert("Invalid login credentials.");
+            document.getElementById("credential_err").textContent="Sorry, your Credentials was incorrect. Please double-check your Credentials."
         }
     });
--- JavaScript Project/GAME JS/signup/signup.js
+++ JavaScript Project/GAME JS/signup/signup.js
@@ -1,26 +1,53 @@
+let btn = document.getElementById('show');
+let usererror = document.getElementById("Username_error")
+let email_error = document.getElementById("Email_error")
+let password_error = document.getElementById("password_error");
+btn.addEventListener('click', () => {
+    let inp = document.getElementById('signupPassword')
+    if (inp.type === 'password') {
+        inp.type = "text";
+        btn.textContent = 'Hide'
+    }
+    else {
+        inp.type = 'password'
+        btn.textContent = "Show"
+    }
+})
 const signupSection = document.getElementById("signupSection");
     document.getElementById("signupForm").addEventListener("submit", (e) => {
     e.preventDefault();
-
-    const name = document.getElementById("name").value.trim();
-    const email = document.getElementById("email").value.trim();
-    const password = document.getElementById("password").value;
+    
+    const name = document.getElementById("signupUsername").value.trim();
+    const email = document.getElementById("signupEmail").value.trim();
+    const password = document.getElementById("signupPassword").value;
 
     const nameRegex = /[A-Za-z]{3,}$/;
     const emailRegex = /^[a-zA-Z0-9.]+@[a-z]+\.[a-z]{2,}$/;
     const passwordRegex =  /^(?=(.*[a-z]){1,})(?=(.*[A-Z]){1,})(?=(.*[0-9]){1,})(?=(.*[@!#$%^&*()_+=><,.?/-]){1,})[a-zA-z0-9@!#$%^&*()_+=><,.?/-]{8,}$/
 
     if (!nameRegex.test(name)) {
-        alert("Name must have only uppercase and lower case letters and min 3 characters.");
+        usererror.textContent="Name must have only uppercase and lower case letters and min 3 characters."
         return;
+    }
+    else
+    {
+        usererror.textContent=""
     }
     if (!emailRegex.test(email)) {
-        alert("Invalid email format.");
+        email_error.textContent="Invalid email format."
         return;
     }
+    else
+    {
+        email_error.textContent=""
+    }
     if (!passwordRegex.test(password)) {
-        alert("Password must have uppercase, lowercase, a number, a symbol, and be at least 8 characters long.");
+        password_error.textContent="Password must have uppercase, lowercase, a number, a symbol, and be at least 8 characters long.";
         return;
+    }
+    else
+    {
+        password.textContent=""
     }
     alert("Sign-up successful! Please login.");
     let users = JSON.parse(localStorage.getItem("Login")) || []
